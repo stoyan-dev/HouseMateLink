0980896329051e427d3e74faa09de7fe20d6bf90
KaterinaSSlavova <k.slavova@student.fontys.nl>
2024-12-04 11:54:24 +0100

    adding class Complaints

---
+++ HouseMateLink/HouseMateLink/Complaint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseMateLink
+{
+    public class Complaint
+    {
+        private const int duration = 7;
+        private DateTime today;
+        public string Description { get; set; }
+        public Complaint(string description)
+        {
+            this.Description = description;
+            today= DateTime.Today;
+        }
+    }
+}
