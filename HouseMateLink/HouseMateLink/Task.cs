1708abfb838a968506d2f0ce12a41260e542858c
stoyan-dev <xuij777@gmail.com>
2024-12-04 11:39:03 +0100

    Creating Task Class and Task Type

---
+++ HouseMateLink/HouseMateLink/Task.cs
@@ -0,0 +1,21 @@
+﻿using HouseMateLink;
+
+public class Task
+{
+    public int TaskID { get; set; }
+    public string TaskDescription { get; set; }
+    public int UserID { get; set; }
+    public DateTime TaskDate { get; set; }
+    public bool IsDone { get; set; }
+    public TaskTypeEnum TaskType { get; set; }  
+
+    public Task(int taskId, string taskDescription, int userId, DateTime taskDate, bool isDone, TaskTypeEnum taskType)
+    {
+        TaskID = taskId;
+        TaskDescription = taskDescription;
+        UserID = userId;
+        TaskDate = taskDate;
+        IsDone = isDone;
+        TaskType = taskType;
+    }
+}
---
+++ HouseMateLink/HouseMateLink/TaskType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseMateLink
+{
+    public enum TaskTypeEnum
+    {
+        CLEAN_BATHROOM,
+        CLEAN_KITCHEN,
+        CLEAN_COMMON_AREA,
+        BUY_GROCERIES,
+        THROW_TRASH
+    }
+}
