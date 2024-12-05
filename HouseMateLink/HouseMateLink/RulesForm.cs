5db87ca6727d13a97441405cb0a86fc769441c18
PGocheva <p.gocheva@student.fontys.nl>
2024-12-05 10:01:20 +0100

    edited the rules

--- HouseMateLink/HouseMateLink/RulesForm.cs
+++ HouseMateLink/HouseMateLink/RulesForm.cs
@@ -21,14 +21,16 @@ namespace HouseMateLink
         {
             string houseRules = @"House Rules:
 
-1. Respect each other's space.
-2. Clean up after yourself.
-3. No loud music after 11 PM.
-4. Take out the trash on designated days.
-5. Don't throw any objects in the toilet.
-6. Shared expenses should be paid on time.
-7. Keep common areas tidy.
-8. No smoking in shared areas.";
+               1. Respect each other's space.
+               2. Always clean up after yourself in shared spaces like the kitchen, living room, and bathroom.
+               3. Maintain silence between 10 PM and 8 AM on weekdays to ensure everyone can study or rest.
+               5. Follow the rotating schedule for shared chores like taking out the trash, cleaning the common 
+               areas.
+               6. Turn off lights, water taps, and electronics when not in use to save energy and reduce bills.
+               7. The use of illegal drugs or substances is strictly prohibited within the housing premises.
+               8. Smoking is prohibited inside the building. Use designated smoking areas outside.
+               9. Guests must respect the house rules and not disturb others.";
+            
 
             rulesTextBox.Text = houseRules;
         }
