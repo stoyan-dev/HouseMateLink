8ad76c9aeb8f9476d2660f87028d091b2996a5a1
Mirela28 <mirelagirleanu@gmail.com>
2024-12-11 12:54:00 +0100

    Helper Class

---
+++ HouseMateLink/HouseMateLink/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseMateLink
+{
+    public class Helper
+    {
+        string connStr = "Server=mssqlstud.fhict.local;Database=dbi550238;User Id=dbi550238;Password=12345;";
+    }
+}
