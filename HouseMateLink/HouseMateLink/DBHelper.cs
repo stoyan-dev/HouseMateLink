9a1e35b14071987dc9a18e278cef9f82dcec0496
Mirela28 <mirelagirleanu@gmail.com>
2024-12-12 10:21:07 +0100

    erg

--- HouseMateLink/HouseMateLink/DBHelper.cs
+++ HouseMateLink/HouseMateLink/DBHelper.cs
@@ -17,7 +17,7 @@ namespace HouseMateLink
             conn = new SqlConnection(connStr);
         }
 
-        public void InitDB()
+        public void Init_db()
         {
             //Delete all the data from the tables
             string deleteAllSql = """
@@ -32,7 +32,31 @@ namespace HouseMateLink
             deleteAllCmd.ExecuteNonQuery();
             conn.Close();
 
+            //Add only the admin account
+            string addAdminSql = """
+                    INSERT INTO USER (Username, Password, Name, IsAdmin, RoomNumber) 
+                    VALUES('admin', 'admin123','Robert', 1, 0);
+                """;
+            SqlCommand addAdminCmd = new SqlCommand(addAdminSql, conn);
+            conn.Open();
+            addAdminCmd.ExecuteNonQuery();
+            conn.Close();
+        }
 
+        public void AddTenantToDB(User u)
+        {
+            string addTenantSql = """
+                    INSERT INTO USER (Username, Password, Name, IsAdmin, RoomNumber)
+                    VALUES(@username, @password, @name, 0, @roomnr)
+                """;
+            SqlCommand addTenantCmd = new SqlCommand(addTenantSql, conn);
+            addTenantCmd.Parameters.AddWithValue("username", u.Username);
+            addTenantCmd.Parameters.AddWithValue("password", u.Password);
+            addTenantCmd.Parameters.AddWithValue("name", u.Name);
+            addTenantCmd.Parameters.AddWithValue("roomnr", u.RoomNumber);
+            conn.Open();
+            addTenantCmd.ExecuteNonQuery();
+            conn.Close();
 
         }
 
